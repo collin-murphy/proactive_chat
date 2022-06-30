eef72ae29fa3da063677e88fade5d5843f57e739
Collin Murphy <55554147+collin-murphy@users.noreply.github.com>
2022-06-30 15:08:16 -0600

    Update proactive_chat.js

--- proactive_chat.js
+++ proactive_chat.js
@@ -1,21 +1,22 @@
-<!-- Source: https://docs.microsoft.com/en-us/dynamics365/customer-service/start-proactive-chat -->
-<!-- Code to show proactive chat invite after visitor has spend given time on the webpage -->
-<script id="Proactivechattrigger">
-	// Wait for Chat widget to load completely
-    window.addEventListener("lcw:ready", function handleLivechatReadyEvent(){
-		var timeToWaitBeforeOfferingProactiveChatInMilliseconds = 20000;//time to wait before Offering proactive chat to webpage visitor
-		// Setting context variables
-        Microsoft.Omnichannel.LiveChatWidget.SDK.setContextProvider(function contextProvider(){
-            return {
-                'Proactive Chat':{'value':'True','isDisplayable':true},
-                'Time On Page':{'value': timeToWaitBeforeOfferingProactiveChatInMilliseconds ,'isDisplayable':true},
-                'Page URL':{'value': window.location.href,'isDisplayable':true},
-            };
-        });
-		
-		//Display proactive chat invite after 'timeToWaitBeforeOfferingProactiveChatInMilliseconds' milliseconds
-        setTimeout(function(){
-            Microsoft.Omnichannel.LiveChatWidget.SDK.startProactiveChat({message: "Hi! Just checking in to see if I can help answer any questions you may have."}, false)
-        },timeToWaitBeforeOfferingProactiveChatInMilliseconds);
+/*
+Source: https://docs.microsoft.com/en-us/dynamics365/customer-service/start-proactive-chat
+Code to show proactive chat invite after visitor has spend given time on the webpage
+*/
+
+// Wait for Chat widget to load completely
+window.addEventListener("lcw:ready", function handleLivechatReadyEvent() {
+    var timeToWaitBeforeOfferingProactiveChatInMilliseconds = 5000;//time to wait before Offering proactive chat to webpage visitor
+    // Setting context variables
+    Microsoft.Omnichannel.LiveChatWidget.SDK.setContextProvider(function contextProvider() {
+        return {
+            'Proactive Chat': { 'value': 'True', 'isDisplayable': true },
+            'Time On Page': { 'value': timeToWaitBeforeOfferingProactiveChatInMilliseconds, 'isDisplayable': true },
+            'Page URL': { 'value': window.location.href, 'isDisplayable': true },
+        };
     });
-</script>
+
+    //Display proactive chat invite after 'timeToWaitBeforeOfferingProactiveChatInMilliseconds' milliseconds
+    setTimeout(function () {
+        Microsoft.Omnichannel.LiveChatWidget.SDK.startProactiveChat({ message: "This is the proactive chat example text." }, false)
+    }, timeToWaitBeforeOfferingProactiveChatInMilliseconds);
+});
